1dc95d4137c223c952272e949d1581ea6f516dac
Maan-netizen <143368784+Maan-netizen@users.noreply.github.com>
2025-09-01 16:20:18 +0530

    code files

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC9jQ3sNAlRf4ihySXN3_YRgDZy_AXcN4E",
+  authDomain: "student-teacher-89c98.firebaseapp.com",
+  projectId: "student-teacher-89c98",
+  storageBucket: "student-teacher-89c98.firebasestorage.app",
+  messagingSenderId: "89895966265",
+  appId: "1:89895966265:web:b1012da686b0369dfb5fe8",
+  measurementId: "G-KVJVN597GM"
+};
